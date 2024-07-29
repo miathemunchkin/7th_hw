432386569c6fa846bb0be56cd190cf4d256d957f
miathemunchkin <mia.rokosowski@gmail.com>
2024-07-29 22:46:38 +0300

    seed.py created

--- seed.py
+++ seed.py
@@ -1,83 +1,72 @@
-import psycopg
 import random
 from faker import Faker
-from random import choice, randint
-from datetime import datetime, timedelta
 from db import session 
-from models import Teacher 
+from models import Teacher, Group, Subject, Grade, Student 
 
-fake = Faker()
+faker = Faker()
 
 def create_fake_teachers(num):
     list_ids = []
     for _ in range(num): 
-        name = fake.name()
+        name = faker.name()
         new_teacher = Teacher(fullname=name)
         session.add(new_teacher)
         session.commit()
         list_ids.append(new_teacher.id)
     return list_ids
     
-def create_fake_groups(cursor, num):
+def create_fake_groups(num):
     list_ids = []
     for i in range(num):
-        name = "group" + str(i)
-        cursor.execute('INSERT INTO "group" (name) VALUES (%s) RETURNING id', (name,))
-        id = cursor.fetchone()[0]
-        list_ids.append(id)
+        name = f"group{i}"
+        new_group = Group(name=name)
+        session.add(new_group)
+        session.commit()
+        list_ids.append(new_group.id)
     return list_ids
 
-def create_fake_students(cursor, num, group_ids):
+def create_fake_students(num, group_ids):
     list_ids = []
-    for _ in range(num):  
-        name = fake.name()
-        group_id = random.choice(group_ids)
-        cursor.execute('INSERT INTO student (name, group_id) VALUES (%s, %s) RETURNING id',
-            (name, group_id))
-        id = cursor.fetchone()[0]
-        list_ids.append(id)
+    for _ in range(num): 
+        new_student = Student(fullname=faker.name(), group_id=random.choice(group_ids))
+        session.add(new_student)
+        session.commit()
+        list_ids.append(new_student.id)
     return list_ids
-
         
-def create_fake_subjects(cursor, num, teacher_ids):
+def create_fake_subjects(num, teacher_ids):
     list_ids = []
     for i in range(num):
-        name = "subject" + str(i)
-        teacher_id = random.choice(teacher_ids)
-        cursor.execute('INSERT INTO subject (name, teacher_id) VALUES (%s, %s) RETURNING id',
-            (name, teacher_id))
-        id = cursor.fetchone()[0]
-        list_ids.append(id)
+        name = f"subject{i}"
+        new_subject = Subject(name=name, teacher_id=random.choice(teacher_ids))
+        session.add(new_subject)
+        session.commit()
+        list_ids.append(new_subject.id)
     return list_ids
         
-def create_fake_grades(cursor, num, student_ids, subject_ids):
+def create_fake_grades(num, student_ids, subject_ids):
     for student_id in student_ids:
         for _ in range(num):
-            value = random.randint(1, 10)
-            date = fake.date()
-            subject_id = random.choice(subject_ids)
-            cursor.execute('INSERT INTO grade (student_id, subject_id, value, date) VALUES (%s, %s, %s, %s)',
-                (student_id, subject_id, value, date))
+            new_grade = Grade(
+                student_id=student_id,
+                subject_id=random.choice(subject_ids),
+                grade=random.uniform(60, 100),
+                date=faker.date_between(start_date='-2y', end_date='today')
+            )
+            session.add(new_grade)
+    session.commit()
         
         
 if __name__ == '__main__':
     print("going to create fake data")
     teacher_ids = create_fake_teachers(5)
     print("teachers created")
-
-    # with psycopg.connect(**conn_params) as conn:
-    #     with conn.cursor() as cur:    
-    #         print("going to create fake data")
-    #         teacher_ids = create_fake_teachers(cur, 5)
-    #         print("teachers created")
-    #         group_ids = create_fake_groups(cur, 3)
-    #         print("groups created")
-    #         student_ids = create_fake_students(cur, 50, group_ids)
-    #         print("students created")
-    #         subject_ids = create_fake_subjects(cur, 8, teacher_ids)
-    #         print("sybjects created")
-    #         create_fake_grades(cur, 20, student_ids, subject_ids)
-    #         print("grades created")
-    #         conn.commit()
-    #         print("Success.")
-
+    group_ids = create_fake_groups(3)
+    print("groups created")
+    student_ids = create_fake_students(50, group_ids)
+    print("students created")
+    subject_ids = create_fake_subjects(8, teacher_ids)
+    print("subjects created")
+    create_fake_grades(20, student_ids, subject_ids)
+    print("grades created")
+    print("Success.")
